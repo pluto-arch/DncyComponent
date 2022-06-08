8cacfecc7a594b6d9b0474586233670fe7f6fd2a
张玉龙 <620524a2924.cdb@sina.cn>
2022-06-08 17:41:56 +0800

    修复错误

--- Dncy.MultiTenancy/Model/TenantInfo.cs
+++ Dncy.MultiTenancy/Model/TenantInfo.cs
@@ -5,6 +5,9 @@ namespace Dncy.MultiTenancy.Model
     [Serializable]
     public class TenantInfo
     {
+        public TenantInfo()
+        {
+        }
         public TenantInfo(string id, string name = null)
         {
             Id = id;
