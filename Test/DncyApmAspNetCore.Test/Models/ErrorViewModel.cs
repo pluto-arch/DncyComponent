f8f01df0660cbea7f1fff1a4f22668952143d988
张玉龙 <620524a2924.cdb@sina.cn>
2023-01-03 10:31:55 +0800

    删除无用项目

--- Test/DncyApmAspNetCore.Test/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DncyApmAspNetCore.Test.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
